c99ca2b6e758ef17780c2804fcdf457d22c0c06e
tranchi0120 <tranchi20012000@gmail.com>
2023-07-03 16:47:07 +0700

    done cart

--- src/feature/layouts/Sidebar/Sidebar.tsx
+++ src/feature/layouts/Sidebar/Sidebar.tsx
@@ -12,26 +12,29 @@ const Sidebar = ({ isShow, handleShowCategoryList }: IProps) => {
   const { Allcategories, isLoading } = useAppSelector(selectorCategories)
 
   return (
-    <div className=''>
+    <div className='scrollbar' id='style-1'>
       <ul
-        className={`duration-300 w-[280px] flex flex-col justify-between mt-20px fixed top-0 left-0 w-280px overflow-scroll !m-0 bg-bgr shadow-3xl text-red-bold bottom-0 z-50 pl-6 pt-7 ${
+        className={`force-overflow duration-300 w-[280px] flex flex-col gap-6 mt-20px fixed top-0 left-0 w-280px overflow-scroll !m-0 bg-bgr shadow-3xl text-red-bold bottom-0 z-50 pl-6 pt-7 ${
           isShow ? 'translate-x-0' : '-translate-x-full '
         }`}
       >
+        <h3 className='text-black'>ALL CATEGORY</h3>
         {isLoading ? (
           <div>loading...</div>
         ) : (
           Allcategories?.map((category, index) => (
-            <Link
-              to={`/categories/${category}`}
-              key={index}
-              className="cursor-pointer relative before:duration-300 flex items-center gap-2
+            <li>
+              <Link
+                to={`/categories/${category}`}
+                key={index}
+                className="cursor-pointer relative before:duration-300 flex items-center gap-2
               before:absolute before:content-[''] before:w-full before:h-[2px] before:text-red-800
               before:scale-[0] before:origin-left before:-bottom-[5px] hover:before:scale-[1] before:bg-red-bold hover:pl-4"
-              onClick={() => handleShowCategoryList()}
-            >
-              <VscDiffAdded /> {category}
-            </Link>
+                onClick={() => handleShowCategoryList()}
+              >
+                <VscDiffAdded /> {category}
+              </Link>
+            </li>
           ))
         )}
       </ul>
