914244cc6b8b165de808fd4b604350f35d2ffe6c
tranchi0120 <tranchi20012000@gmail.com>
2023-07-04 11:17:24 +0700

    fix sidabar

--- src/feature/layouts/Sidebar/Sidebar.tsx
+++ src/feature/layouts/Sidebar/Sidebar.tsx
@@ -15,7 +15,7 @@ const Sidebar = ({ isShow, handleShowCategoryList }: IProps) => {
   return (
     <div className='scrollbar' id='style-1'>
       <ul
-        className={`force-overflow duration-300 w-[280px] flex flex-col gap-6 mt-20px fixed top-0 left-0 w-280px overflow-scroll !m-0 bg-bgr shadow-3xl text-red-bold bottom-0 z-50 pl-6 pt-7 ${
+        className={`force-overflow duration-300 w-[280px] flex flex-col gap-6 mt-20px fixed top-0 left-0 w-280px overflow-scroll !m-0 bg-bgr shadow-3xl text-black bottom-0 z-50 pl-6 pt-7 ${
           isShow ? 'translate-x-0' : '-translate-x-full '
         }`}
       >
@@ -32,8 +32,8 @@ const Sidebar = ({ isShow, handleShowCategoryList }: IProps) => {
                 to={`/categories/${category}`}
                 key={index}
                 className="cursor-pointer relative before:duration-300 flex items-center gap-2
-              before:absolute before:content-[''] before:w-full before:h-[2px] before:text-red-800
-              before:scale-[0] before:origin-left before:-bottom-[5px] hover:before:scale-[1] before:bg-red-bold hover:pl-4"
+              before:absolute before:content-[''] before:w-full before:h-[2px] before:text-black
+              before:scale-[0] before:origin-left before:-bottom-[5px] hover:before:scale-[1] before:bg-black hover:pl-4"
                 onClick={() => handleShowCategoryList()}
               >
                 <VscDiffAdded /> {category}
