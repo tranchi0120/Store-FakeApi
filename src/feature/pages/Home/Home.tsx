fd40c365b3a6421563778538299f0073144dcef0
tranchi0120 <tranchi20012000@gmail.com>
2024-06-30 15:41:24 +0700

    refactor component Home.tsx

--- src/feature/pages/Home/Home.tsx
+++ src/feature/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ import { selectorCategories } from '../../../redux/slice/CategorySlice'
 import { IProduct } from '../../../types/interfaces'
 import ProductItem from '../ProductItem/ProductItem'
 import Hero from '../../layouts/Hero/Hero'
-import Sidebar from '../../layouts/Sidebar/Sidebar'
+import Sidebar from './../../layouts/Sidebar/Sidebar'
 import { Pagination } from '@mui/material'
 import { RiFunctionLine } from 'react-icons/ri'
 
@@ -22,7 +22,7 @@ const Home = () => {
     setIsShow(!isShow)
   }
 
-  const limit = 10
+  let limit = 10
 
   /*pagination */
   const totalPages = Math.ceil(products.length / limit)
@@ -39,10 +39,13 @@ const Home = () => {
     setCurrentPage(page)
   }
 
+  /* call products by category */
   if (Allcategories.length === 0 && products.length === 0) {
     return null
   }
 
+  console.log('Allcategories', Allcategories)
+  console.log('products', products)
   const productCategories = Allcategories.map((category) => {
     return products.filter((product: IProduct) => product.category === category.slug)
   })
@@ -50,27 +53,35 @@ const Home = () => {
   return (
     <div>
       <Hero />
-      <div className='container'>
+      <div className='container  '>
         <div>
-          <div className='hidden max-[450px]:block cursor-pointer mt-3 w-[30px] h-[30px]' onClick={handleShowCategory}>
+          <div
+            className='hidden max-[450px]:block cursor-pointer mt-3 w-[30px] h-[30px] '
+            onClick={() => handleShowCategory()}
+          >
             <RiFunctionLine size='30px' color='black' />
           </div>
-          <div className='p-[20px] bg-brand mt-8 font-[600] text-[22px] border-l-[6px] border-black'>
-            SEE OUR PRODUCT
+          <div className='p-[20px] bg-brand mt-8 font-[600] text-[22px] border-l-[6px] border-black '>
+            SEE OUT PRODUCT
           </div>
           {isLoading && <Loader />}
           <div className='flex gap-3'>
-            <div className='relative'>
+            <div className=' relative'>
               <Sidebar isShow={isShow} />
             </div>
-            <div className='grid xl:grid-cols-3 gap-8 mt-6 lg:grid-cols-2 sm:grid-cols-2'>
-              {currentProducts.map((product) => (
-                <ProductItem key={product.id} id={product.id} product={product} />
-              ))}
+            <div className='grid xl:grid-cols-3 gap-8 mt-6 lg:grid-cols-2 sm:grid-cols-2  '>
+              {currentProducts.map((product) => {
+                return <ProductItem key={product.id} id={product.id} product={product} />
+              })}
             </div>
           </div>
+
+          <div className='flex items-center justify-center mt-6'>
+            <Pagination count={totalPages} shape='rounded' page={currentPage} onChange={handlePageChange} />
+          </div>
+
           {!isLoading &&
-            productCategories.map((categoryProducts, index) => (
+            productCategories.slice(0, 4).map((categoryProducts, index) => (
               <div key={index}>
                 <div className='p-[20px] bg-brand mt-8 font-[600] text-[22px] border-l-[6px] border-black'>
                   {Allcategories[index]?.name.toUpperCase()}
@@ -82,9 +93,6 @@ const Home = () => {
                 </div>
               </div>
             ))}
-          <div className='flex items-center justify-center mt-6'>
-            <Pagination count={totalPages} shape='rounded' page={currentPage} onChange={handlePageChange} />
-          </div>
         </div>
       </div>
     </div>
